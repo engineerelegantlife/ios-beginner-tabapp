74d0967e27806732347faf933180437550c0c7d0
Tomo Shimizu <tomoshimizu@TomoMac.local>
2022-10-28 18:24:29 +0900

    first commit

---
+++ TabApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabApp
+//
+//  Created by Tomo Shimizu on 2022/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
